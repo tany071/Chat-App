730634a31bb6647918876b0414f6bafb401028aa
tany071 <tanyasinha7u@gmail.com>
2023-06-19 23:24:11 +0530

    register auth

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth"
+import { getStorage} from "firebase/storage";
+
  
 const firebaseConfig = {
   apiKey: "AIzaSyD9bMSyFGE8Utz8vGTHF8Ibnac3c7Vi05I",
@@ -13,3 +15,4 @@ const firebaseConfig = {
 
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth();
+export const storage = getStorage();
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,47 +1,56 @@
-import React from 'react';
-import { createUserWithEmailAndPassword } from "firebase/auth";
-import { auth } from "../firebase";
+import React, { useState } from "react";
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { storage, auth } from "../firebase";
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 
 const Register = () => {
+  const [err, setErr] = useState(false);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const displayname = e.target[0].value;
+    const email = e.target[1].value;
+    const password = e.target[2].value;
 
-    const handleSubmit = (e)=>{
-        e.preventDefault()
-        const displayname = e.target[0].value;
-        const email = e.target[1].value;
-        const password = e.target[2].value;
+    try {
+      const res = await createUserWithEmailAndPassword(auth, email, password);
+      const storageRef = ref(storage, displayname);
+      const uploadTask = uploadBytesResumable(storageRef, file);
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+           
+        },
+        (error) => {
+          // Handle unsuccessful uploads
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then(async (downloadURL) => {
+             await updateProfile(res.user ,{
+              displayname
+             } )
+          });
+        }
+      );
+    } catch (error) {
+      setErr(true);
     }
-
-
-    
-    createUserWithEmailAndPassword(auth, email, password)
-      .then((userCredential) => {
-        // Signed in 
-        const user = userCredential.user;
-        console.log(user);
-        // ...
-      })
-      .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        // ..
-      });
-
+  };
 
   return (
-    <div className='formContainer'>
-        <div className='formWrapper'>
-            <div className='title'>Register</div>
-            <form onSubmit={handleSubmit}>
-                <input type='text' placeholder='display name'/>
-                <input type='email' placeholder='email'/>
-                <input type='password' placeholder='password'/>
-                <button>Sign Up</button>
-            </form>
-            <p>Already have an account? Login</p>
-
-        </div>
+    <div className="formContainer">
+      <div className="formWrapper">
+        <div className="title">Register</div>
+        <form onSubmit={handleSubmit}>
+          <input type="text" placeholder="display name" />
+          <input type="email" placeholder="email" />
+          <input type="password" placeholder="password" />
+          <button>Sign Up</button>
+          {err && <span>Something went wrong</span>}
+        </form>
+        <p>Already have an account? Login</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Register;
